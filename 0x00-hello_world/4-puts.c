f65d5eb4da6306dd753aee3ed10e944c867b2aa7
Babatunde adam <adamlanre224@gmail.com>
2022-09-08 08:35:19 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - print the string with put function
- * return: 0 if exited properly, non-zero otherwise
-*/
-int main(void) 
+ * main - print the string in the puts function
+ *
+ * Description: using the main function
+ * this program prints Programming a is like building multilingual puzzle
+ * Return: 0
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
