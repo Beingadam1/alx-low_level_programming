31da568c936556d66e7b6ea1da7942e6d9b974ad
Babatunde adam <adamlanre224@gmail.com>
2022-09-19 12:21:22 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -15,13 +15,9 @@ void puts2(char *str)
 
 	while (*(str + i) != '\0')
 	{
+		if (i % 2 == 0)
+			putchar(*(str + i));
 		i++;
 	}
-
-	if (i % 2 == 0)
-	{
-		putchar(*(str + i));
-	}
-
-	putchar('\n');
+	putchar(10);
 }
