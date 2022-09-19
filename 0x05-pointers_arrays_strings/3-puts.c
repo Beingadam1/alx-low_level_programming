8cdb1791820868cb5d2d782470f44b37f0404ec3
Babatunde adam <adamlanre224@gmail.com>
2022-09-19 09:05:22 -0700

    3-puts.c updated

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,10 +8,12 @@
 
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(*str);
+		putchar(str[i]);
 	}
 
-	_putchar('\n');
+	putchar('\n');
 }
