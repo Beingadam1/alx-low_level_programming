31c973bcef8ee28706f078ed9c5d4d261abc3c91
Babatunde adam <adamlanre224@gmail.com>
2022-09-19 12:01:11 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,6 +14,7 @@ char *_strcpy(char *dest, char *src)
 	int len;
 	int i;
 
+	len = 0;
 	while (src[len] != '\0')
 	{
 		len++;
@@ -26,5 +27,5 @@ char *_strcpy(char *dest, char *src)
 
 	dest[i] = '\0';
 
-	return (0);
+	return (dest);
 }
