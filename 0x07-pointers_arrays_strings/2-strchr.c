2dd92dc2cae27dd9ed0b6941164d93c3627e1764
Babatunde adam <adamlanre224@gmail.com>
2022-09-26 14:28:56 -0700

    2-strchr.c added

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr -  locates a character in a string
+ * @s: string to search
+ * @c: char to search for
+ * Return: string from character
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		else
+		{
+			return ('\0');
+		}
+
+		s++;
+	}
+
+}
