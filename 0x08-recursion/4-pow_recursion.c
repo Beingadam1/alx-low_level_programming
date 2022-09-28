e87fecd61554f62f9ff651f22417724b6e0fb4d3
Babatunde adam <adamlanre224@gmail.com>
2022-09-28 09:08:00 -0700

    betty error fixed

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,6 +4,7 @@
 * _pow_recursion -  returns the value of x raised to the power of y
 * @x: the value to multiply
 * @y: number of times to multiply the value
+* Return: -1 if y < 0, 1 if y == 0
 */
 
 int _pow_recursion(int x, int y)
